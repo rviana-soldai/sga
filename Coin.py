b3435f5b89252d006da0b0faa42e6efd4e66f609
rviana-soldai <58533653+rviana-soldai@users.noreply.github.com>
2019-12-10 13:52:37 -0600

    Documented

--- Coin.py
+++ Coin.py
@@ -1,9 +1,52 @@
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Coin
+
+This class represent a probabilityof a coin
+
+This file can also be imported as a module and contains the following
+methods:
+    *toss - return true or false depending if the random value is equal
+    or less than the probability variable
+
+"""
 import random
 
+
 class Coin:
+    """
+    A class used to represent a coin probability
 
-	def __init__(self, probability):
-		self.probability = probability
+    Attributes
+    ----------
+    probability : float
+       The probability threshold
+   
 
-	def toss(self):
-		 return random.random() <= self.probability
+    Methods
+    -------
+    toss 
+        return true or false if the value get in random is equal or less 
+        than probability threshold
+    """
+    def __init__(self, probability):
+        """
+        Parameters
+        ----------
+        probability : float
+           The probability threshold
+
+        """
+        self.probability = probability
+
+    def toss(self):
+        """Return true or false if the value get in random is equal or less 
+        than probability threshold
+
+        Return
+        ------
+        bool
+         True if random value is less or equal than probability treshold and false 
+         in other case
+        """
+        return random.random() <= self.probability
